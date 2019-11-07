aec59502112b70eadb48950d3b61b7b2f5b1b053
juanagustin0504 <juanagustin@naver.com>
2019-11-07 17:31:13 +0900

    add : touches moved

--- Sketch/ViewController.swift
+++ Sketch/ViewController.swift
@@ -32,7 +32,24 @@ class ViewController: UIViewController {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
+        UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
+        UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
         
+        let touch = touches.first! as UITouch
+        let currPoint = touch.location(in: imgView)
+        
+        imgView.image?.draw(in: CGRect(x: 0, y: 0, width: imgView.frame.size.width, height: imgView.frame.size.height))
+        
+        UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: currPoint.x, y: currPoint.y))
+        UIGraphicsGetCurrentContext()?.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        lastPoint = currPoint
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
