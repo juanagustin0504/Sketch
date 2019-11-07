bc878408c5cd00eb7da32cf14b5cdb8d18a30956
juanagustin0504 <juanagustin@naver.com>
2019-11-07 17:27:10 +0900

    add : btn clear, touches began

--- Sketch/ViewController.swift
+++ Sketch/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var lastPoint : CGPoint!
+    var lineSize : CGFloat = 2.0
+    var lineColor = UIColor.red.cgColor
 
+    @IBOutlet weak var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnClearImageView(_ sender: UIButton) {
+        imgView.image = nil
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        lastPoint = touch.location(in: imgView)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
 }
 
